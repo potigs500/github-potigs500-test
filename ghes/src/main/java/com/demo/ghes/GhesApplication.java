c9358f8386d965b2841459ed4703e6da845ebef4
potora <potorad@gmail.com>
2024-07-24 10:14:25 +0200

    project init

---
+++ ghes/src/main/java/com/demo/ghes/GhesApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ghes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhesApplication.class, args);
+	}
+
+}
---
+++ ghes/src/test/java/com/demo/ghes/GhesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ghes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
